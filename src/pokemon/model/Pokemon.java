676512ccb48f386b37a0c15a014d1f6b3280654c
Pbra1484 <pbra1484@740s-pl-257.local>
2016-12-05 13:45:25 -0700

    Added tests and began coding models
    Also redesigned  GUI

--- src/pokemon/model/Pokemon.java
+++ src/pokemon/model/Pokemon.java
@@ -1,5 +1,62 @@
 package pokemon.model;
 
-public class Pokemon {
+public abstract class Pokemon 
+{
+	private int attackPoints;
+	private int healthPoints;
+	private double speed;
+	private String name;
+	private int number;
+	
+	public Pokemon(String name, int number)
+	{
+		
+	}
+	
+	public String getPokemonTypes()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		return "You called toString";
+	}
+	
+	public String getPokemonInformation()
+	{
+		
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	public int getAttackPoints() {
+		return attackPoints;
+	}
+	public void setAttackPoints(int attackPoints) {
+		this.attackPoints = attackPoints;
+	}
+	public int getHealthPoints() {
+		return healthPoints;
+	}
+	public void setHealthPoints(int healthPoints) {
+		this.healthPoints = healthPoints;
+	}
+	public double getSpeed() {
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	public String getName() {
+		return name;
+	}
+
+	
+	
+	
+	
+	
 
 }
